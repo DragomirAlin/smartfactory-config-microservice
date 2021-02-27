3202e4762c0f412ae89b7fc623fe804418f73a61
Dragomir Alin <dragomirdanielalin@gmail.com>
2021-02-27 20:42:19 +0200

    setup configuration

---
+++ src/test/java/ro/dragomiralin/configuration/ConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.dragomiralin.configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
